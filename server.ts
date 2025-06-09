f9c58417309f88ce98fe7b7c6a5a6ba04e8dd701
Davi R <contact@davirds.dev>
2025-06-09 19:11:00 -0300

    starting project

---
+++ server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello,  world')
+})
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`)
+})
