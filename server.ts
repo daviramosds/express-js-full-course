3fe3c3a64dcaf19938541f0b58f05314b2da8fc6
Davi R <contact@davirds.dev>
2025-06-09 20:17:52 -0300

    add: get request

--- server.ts
+++ server.ts
@@ -5,9 +5,25 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello,  world')
+  res.status(200).send('Hello,  world')
 })
 
+app.get('/api/users', (req: Request, res: Response) => {
+  res.status(200).send([
+  { id: '1', username: 'davirds', displayName: 'Davi R' },
+  { id: '2', username: 'mariasilva', displayName: 'Maria Silva' },
+  { id: '3', username: 'joaosantos', displayName: 'João Santos' },
+  { id: '4', username: 'carolpires', displayName: 'Carolina Pires' },
+  { id: '5', username: 'lucasf', displayName: 'Lucas Ferreira' },
+  { id: '6', username: 'anapaula', displayName: 'Ana Paula' },
+  { id: '7', username: 'pedroalves', displayName: 'Pedro Alves' },
+  { id: '8', username: 'julianar', displayName: 'Juliana Ribeiro' },
+  { id: '9', username: 'matheust', displayName: 'Matheus Teixeira' },
+  { id: '10', username: 'lais.cruz', displayName: 'Laís Cruz' },
+  { id: '11', username: 'rafael.moraes', displayName: 'Rafael Moraes' }
+]
+)
+})
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`)
 })
