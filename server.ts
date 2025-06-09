65ea1f3322eb0dbdb59d858bfa342ede32f7a212
Davi R <contact@davirds.dev>
2025-06-09 20:26:48 -0300

    route params

--- server.ts
+++ server.ts
@@ -1,29 +1,48 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const fakeUsers = [
+  { id: 1, username: 'davirds', displayName: 'Davi R' },
+  { id: 2, username: 'mariasilva', displayName: 'Maria Silva' },
+  { id: 3, username: 'joaosantos', displayName: 'João Santos' },
+  { id: 4, username: 'carolpires', displayName: 'Carolina Pires' },
+  { id: 5, username: 'lucasf', displayName: 'Lucas Ferreira' },
+  { id: 6, username: 'anapaula', displayName: 'Ana Paula' },
+  { id: 7, username: 'pedroalves', displayName: 'Pedro Alves' },
+  { id: 8, username: 'julianar', displayName: 'Juliana Ribeiro' },
+  { id: 9, username: 'matheust', displayName: 'Matheus Teixeira' },
+  { id: 10, username: 'lais.cruz', displayName: 'Laís Cruz' },
+  { id: 11, username: 'rafael.moraes', displayName: 'Rafael Moraes' }
+];
+
+interface IUser {
+  id: number;
+  username: string;
+  displayName: string;
+}
+
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello,  world')
-})
+  res.status(200).send('Hello, world');
+});
 
 app.get('/api/users', (req: Request, res: Response) => {
-  res.status(200).send([
-  { id: '1', username: 'davirds', displayName: 'Davi R' },
-  { id: '2', username: 'mariasilva', displayName: 'Maria Silva' },
-  { id: '3', username: 'joaosantos', displayName: 'João Santos' },
-  { id: '4', username: 'carolpires', displayName: 'Carolina Pires' },
-  { id: '5', username: 'lucasf', displayName: 'Lucas Ferreira' },
-  { id: '6', username: 'anapaula', displayName: 'Ana Paula' },
-  { id: '7', username: 'pedroalves', displayName: 'Pedro Alves' },
-  { id: '8', username: 'julianar', displayName: 'Juliana Ribeiro' },
-  { id: '9', username: 'matheust', displayName: 'Matheus Teixeira' },
-  { id: '10', username: 'lais.cruz', displayName: 'Laís Cruz' },
-  { id: '11', username: 'rafael.moraes', displayName: 'Rafael Moraes' }
-]
-)
-})
+  res.status(200).send(fakeUsers);
+});
+
+app.get('/api/users/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const parsedId = parseInt(id);
+
+  const user = fakeUsers.find((user: IUser) => user.id === parsedId);
+
+  if (!user)  res.status(404).send({ message: 'User not found' });
+
+  res.status(200).send(user);
+});
+
 app.listen(PORT, () => {
-  console.log(`Running on Port ${PORT}`)
-})
+  console.log(`Running on Port ${PORT}`);
+});
